0d17c576b56db1e8ac4f71ffd2c966f6e4b97109
Nitin S <atomic.unit@gmail.com>
2013-07-11 16:37:36 -0700

    test code

---
+++ test.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+ public class test {
+  public static void main(String[] args) {
+    JFrame frame = new JFrame("Simple GUI");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    
+    frame.setSize(800, 600);
+    frame.setLocationRelativeTo(null);
+
+    frame.setVisible(true);
+  } 
+ } 
